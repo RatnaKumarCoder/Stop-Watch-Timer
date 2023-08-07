e2aeb3ff7316abfbcfb92c154b4e94921ba33a98
RatnaKumarCoder <141625413+RatnaKumarCoder@users.noreply.github.com>
2023-08-07 15:55:38 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
+// timer elements
 var minutes=document.getElementById("minutes");
 var seconds=document.getElementById("seconds");
 var hours=document.getElementById("hours");
 
+// controls
 var start=document.getElementById("start");
 var pause=document.getElementById("pause");
 var reset=document.getElementById("reset");
 
-
+// variables to keep track of the time.
 let seconds_counter=0;
 let minutes_counter=0;
 let hours_counter=0;
 
+// variable to store the setInterval function id
 let interval;
 
+// timer function that will be initiated through start_timer function
 function timer(){
     if(seconds_counter<10){
         seconds.innerHTML="0"+seconds_counter;
@@ -42,15 +46,18 @@ function timer(){
         hours_counter++;
     }
 }
+
+// start timer function that will be initiated through click event on start button
 function start_timer(){
     interval=setInterval(timer,1000);
 }
 
-
+// pause timer function that will initiated through click event on pause button
 function pause_timer(){
     clearInterval(interval);
 }
 
+// reset timer function that will be initiated through click event on reset button
 function reset_timer(){
     seconds_counter=0;
     minutes_counter=0;
@@ -61,6 +68,7 @@ function reset_timer(){
     pause_timer();
 }
 
+// event listeners specified for start, pause and reset buttons
 start.addEventListener('click',start_timer);
 pause.addEventListener('click',pause_timer);
 reset.addEventListener('click',reset_timer);
